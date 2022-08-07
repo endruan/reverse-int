20bb7cd6a3f17fdbbd570108bd643d682cde5fc7
Andrey T <truannik@mail.ru>
2022-08-07 21:07:47 +0300

    Compleated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
+  let oldNum = n + '';
+  let newNum = '';
+  let len = oldNum.length-1;
   
+
+  if(oldNum[0] === "-") {
+    oldNum = oldNum.replace("-","");
+    len= len-1;
+  }
+
+  for (x in oldNum){
+    newNum += oldNum[len];
+    len = len - 1; 
+  }
+
+   return newNum;
 }
